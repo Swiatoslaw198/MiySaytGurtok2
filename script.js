8d95f33b42717250b209ced9ae9b1629f1f6b546
Swiatoslaw198 <Swiatoslaw81@gmail.com>
2025-01-04 16:42:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$('.your-class').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    slidesToShow: 2,
+  });
+//   console.log("NTST");
+  
